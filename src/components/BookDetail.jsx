781a237a46606731bd474d6548d742b2c62457e9
Stefano Casasola <ste.casasola@gmail.com>
2022-06-02 12:07:15 +0200

    Connects CartIndicator and BookDetail

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,17 +1,37 @@
-import { Component } from "react";
-import { Col, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import { Component } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+// mapDispatchToProps is a function returning an object!
+// every key in the object it's returning will be a prop for the component!
+
+// if the keys of the object mapStateToProps is returning are tipically properties,
+// the keys of the object mapDispatchToProps is returning are typically METHODS
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: () => {
+      dispatch({
+        type: 'ADD_TO_CART',
+      })
+    },
+  }
+}
 
 class BookDetail extends Component {
   state = {
     book: null,
-  };
+  }
 
   componentDidUpdate(prevProps) {
     if (prevProps.bookSelected !== this.props.bookSelected) {
       this.setState({
         book: this.props.bookSelected,
-      });
+      })
     }
   }
 
@@ -44,7 +64,12 @@ class BookDetail extends Component {
                   <span className="font-weight-bold">Price:</span>
                   {this.state.book.price}
                 </p>
-                <Button color="primary" onClick={() => {}}>
+                <Button
+                  color="primary"
+                  onClick={() => {
+                    this.props.addToCart()
+                  }}
+                >
                   ADD TO CART
                 </Button>
               </Col>
@@ -58,8 +83,11 @@ class BookDetail extends Component {
           </Row>
         )}
       </div>
-    );
+    )
   }
 }
 
-export default BookDetail;
+export default connect(mapStateToProps, mapDispatchToProps)(BookDetail)
+
+// what do I need to do from here? do I need just to read or do I need to write?
+// I actually just need to "write" (dispatch an action) from here!
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -15,20 +15,20 @@ const initialState = {
 
 // = initialState means the DEFAULT VALUE of the argument!
 const mainReducer = (state = initialState, action) => {
-    // the reducer function will be in charge of computing the 
-    // new state of the application every time an action gets dispatched
-    // our reducer needs to be told what to do when a particular action gets dispatched
-    // so we have a bunch of possible actions, so let's write a switch case to manage them all!
-    switch(action.type){
-        // case1
-        // case2
-        default:
-            // the default case is for an action.type that we didn't think of
-            // maybe an edge case, maybe a bug, something unhandled...
-            // what is the new state we're going to compute out of this edge case??
-            return state
-            // returning the state as it was in our default case makes the application unbreakable!
-    }
+  // the reducer function will be in charge of computing the
+  // new state of the application every time an action gets dispatched
+  // our reducer needs to be told what to do when a particular action gets dispatched
+  // so we have a bunch of possible actions, so let's write a switch case to manage them all!
+  switch (action.type) {
+    // case 'ADD_TO_CART'
+    // case2
+    default:
+      // the default case is for an action.type that we didn't think of
+      // maybe an edge case, maybe a bug, something unhandled...
+      // what is the new state we're going to compute out of this edge case??
+      return state
+    // returning the state as it was in our default case makes the application unbreakable!
+  }
 }
 
 export default mainReducer
